7743c90c1fc38f8febd681f2005fcae6d2da7a3f
Jorge Martínez <jorgemartinez.jam@gmail.com>
2025-06-24 11:36:23 -0300

    Add protected route component

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+// /components/ProtectedRoute.tsx
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuthStore } from "@/store/authStore";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const { accessToken } = useAuthStore();
+
+  useEffect(() => {
+    if (!accessToken) {
+      router.push("/");
+    }
+  }, [accessToken, router]);
+
+  return <>{accessToken ? children : null}</>;
+}
